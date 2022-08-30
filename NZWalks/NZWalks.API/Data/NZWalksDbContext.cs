5ddd321798730a7a0e7729a304ae27b69b91527c
rackelzz <rachael.leach@htri.net>
2022-08-30 13:57:26 -0500

    End of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+//DbContext represents a sessin with the database
+// 1. Make connection 
+// 2. Query 
+// 3. Persist Data 
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext: DbContext
+    {
+        //ctor tab to create contructor 
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+
+
+    }
+}
