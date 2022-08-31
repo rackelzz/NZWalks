aafc93f62eba715cb923bc6035ef588018f26b97
rackelzz <rachael.leach@htri.net>
2022-08-31 10:50:10 -0500

    before change to async

---
+++ NZWalks/NZWalks.API/Profiles/RegionsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionsProfile : Profile
+    {
+
+        public RegionsProfile()
+        {
+            CreateMap<Models.Domain.Region, Models.DTO.Region>();
+                //.ForMemeber(dest => dest.Id, options => options.MapFrom(src => src.RegionId)); dont need here bc names are same
+                //.ReverseMap();
+        }
+
+
+    }   
+}
---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository   //implementatin must implement all methods defined in interface
+    {
+        private readonly NZWalksDbContext nNZWalksDbContext;
+
+        public RegionRepository(NZWalksDbContext nNZWalksDbContext) // injecting dbcontext through constructor 
+        {
+            this.nNZWalksDbContext = nNZWalksDbContext;
+        }
+
+        public IEnumerable<Region> GetAll()
+        {
+            return nNZWalksDbContext.Regions.ToList();
+        }
+    }
+}
